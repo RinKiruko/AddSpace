0aa2defc6d375871a457136f347caa265a610064
RinKiruko <daniktarasov@gmail.com>
2019-03-22 02:37:34 +0700

    blabla

--- Add/settings.py
+++ Add/settings.py
@@ -130,7 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR + 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static')
+)
 
 LOGIN_REDIRECT_URL = reverse_lazy('AddList:GetAllAdds')
 LOGOUT_REDIRECT_URL = reverse_lazy('AddList:GetAllAdds')
